da4e3df4fa032d659ee4f51412a0f2ceb85dd31f
532758463 <532758463@qq.com>
2018-09-23 09:44:47 +0800

    后台更新9月23日

--- module/admin/admin.js
+++ module/admin/admin.js
@@ -16,25 +16,33 @@ router.post('/addNovel',(req,res)=>{
         console.log("请输入内容");
         return ;
     }
-    let sql = `INSERT INTO books(bname, kinds, author, wordnum) VALUES (?,?,?,?)`;
+    let sql = `INSERT INTO books(bname, kinds, author, wordnum,readnum,cover) VALUES (?,?,?,?,?,?)`;
     // let sql= 'UPDATE books SET bname = ?,kinds=?,auther=?,readnum=?,wordnum=?  WHERE bid = ? AND status=1';
-    let data=[d.bname,d.kinds,d.author,d.wordnum];
-    mydb.query(sql,data,(err,result)=>{
-        if(err){
+    let data = [d.bname, d.kinds, d.author, d.wordnum, d.readnum, d.cover];
+    conn.query(sql, data, (err, result) => {
+        if (err) {
             console.log(err);
             res.json({r:'db_err'});
             return ;
         }
-        res.json({r:'success'});
+        res.json({
+            r: 'success'
+        });
     })
 })
-// 小说详细信息栏目
-router.get('/novelAll',(req,res)=>{
-    res.render('admin/novelAll');
-})
-router.get('/user',(req,res)=>{
-    res.render('admin/user');
-})
+
+// 查询所有用户信息
+router.get('/user', (req, res) => {
+    let data = {};
+    // novel.username = req.session.username;
+    //查询所有小说
+    let sql = 'SELECT * FROM user WHERE status = 1';
+    conn.query(sql, (err, results) => {
+        data.user = results;
+        console.log(results);
+        res.render('admin/user', data);
+    });
+});
 
 // 添加用户栏目
 router.get('/addUser',(req,res)=>{
@@ -47,4 +55,94 @@ router.get('/addUser',(req,res)=>{
 router.get('/personnal',(req,res)=>{
     res.render('admin/personnal');
 })
-module.exports=router;
+
+
+
+// 小说信息栏目
+router.get('/novelAll', (req, res) => {
+    let novel = {};
+    // novel.username = req.session.username;
+    //查询所有小说
+    let sql = 'SELECT * FROM books WHERE status = 1';
+    conn.query(sql, (err, results) => {
+        novel.novelAll = results;
+        console.log(results);
+        res.render('admin/novelAll', novel);
+    });
+});
+
+// 删除小说
+router.get('/delete', (req, res) => {
+    let sql = 'UPDATE books SET status = 0 WHERE bid = ? LIMIT 1';
+    conn.query(sql, req.query.bid, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json({
+                r: 'db_err'
+            });
+            return;
+        }
+        res.json({
+            r: 'success'
+        });
+    });
+});
+
+// 删除用户
+router.get('/delete2', (req, res) => {
+    let sql = 'UPDATE user SET status = 0 WHERE uid = ? LIMIT 1';
+    conn.query(sql, req.query.uid, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json({
+                r: 'db_err'
+            });
+            return;
+        }
+        res.json({
+            r: 'success'
+        });
+    });
+});
+
+// 配置小说章节管理界面
+router.get('/addChapter', (req, res) => {
+
+    res.render('admin/addChapter');
+})
+// router.get('/novelAll/chapter', (req, res) => {
+
+//     res.render('admin/chapter');
+// })
+// 查询当前小说目录下的章节信息
+router.get('/novelAll/chapter', (req, res) => {
+    let data = {};
+    console.log(req.query.bid)
+    let bid=req.query.bid;
+    // novel.username = req.session.username;
+    //查询所有小说
+    let sql = `SELECT * FROM section WHERE belong = ${bid}`;
+    conn.query(sql, (err, results) =>{
+      
+        if (err) {
+            console.log(err);
+            res.json({
+                r: 'db_err'
+            });
+            return;
+        }  
+        data.chapterlist = results;
+        console.log(results);
+      
+        res.render('admin/chapter',data);
+        //   res.json({
+        //     r: 'success'
+        // });
+    });
+    // conn.query(sql, (err, results) => {
+    //     novel.novelAll = results;
+    //     console.log(results);
+    //     res.render('admin/novelAll', novel);
+    // });
+})
+module.exports = router;
